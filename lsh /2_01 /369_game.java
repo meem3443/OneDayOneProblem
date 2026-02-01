6234fd99b87417e1b278d8f5921fcd4ff081e4c1
lshlsh2523 <lshlsh2523@naver.com>
2026-02-01 22:02:45 +0900

    Update and rename problem_1.java to 369_game.java

---
+++ lsh /2_01 /369_game.java
@@ -0,0 +1,22 @@
+/*
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i=1; i<=N; i++) {
+            if(i%10==3 || i%10==6 || i%10==9) {           // 일의자리 수만 3, 6, 9인지 검사하고 있음. 틀린 코드!
+                System.out.print("0"+" ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+*/
+
