109d31f538d5e05ab758aff924c593fb41cf55bf
은 <162540155+Eun0503@users.noreply.github.com>
2026-03-22 00:26:20 +0900

    2026_03_21_problem_1_solved

--- Je/0321/겹치지 않는 두 직사각형.py
+++ Je/0321/겹치지 않는 두 직사각형.py
@@ -15,33 +15,37 @@ def kadane(arr):
 
 
 def calc(grid, n, m):
-    #최대 직사각형의 합 갱신
+    # 최대 직사각형의 합 갱신
     best = [-float('inf')] * (n + 1)
 
     for r1 in range(n):
         col_sum = [0] * m
         for r2 in range(r1, n):
             for j in range(m):
-                col_sum[j] += grid[r2][j]   # 칼럼 합치기
+                col_sum[j] += grid[r2][j]  # 칼럼 합치기
 
-            best[r2 + 1] = max(best[r2], kadane(col_sum))   # 카데인 돌림
+            best[r2 + 1] = max(best[r2 + 1], kadane(col_sum))  # 카데인 돌림
+
+    for i in range(1, n + 1):
+        best[i] = max(best[i], best[i - 1])
 
     return best
 
 
 top    = calc(grid, n, m)
-bottom = calc(grid[::-1], n, m)     #격자 뒤집어서 활용
+bottom = calc(grid[::-1], n, m)     # 격자 뒤집어서 활용
 
 grid_t = list(zip(*grid))
-left  = calc(grid_t, m, n)         
-right = calc(grid_t[::-1], m, n)    #격자 뒤집어서 활용
+left  = calc(grid_t, m, n)
+right = calc(grid_t[::-1], m, n)    # 격자 뒤집어서 활용
 
 
-ans = top[1] + bottom[n - 1]  # 수평 첫 번째 분할
+ans = -float('inf')
 
-for k in range(1, n):   #수형분할
+for k in range(1, n):   # 수평분할
     ans = max(ans, top[k] + bottom[n - k])
-for k in range(1, m):   #수직분할
+
+for k in range(1, m):   # 수직분할
     ans = max(ans, left[k] + right[m - k])
 
 print(ans)
