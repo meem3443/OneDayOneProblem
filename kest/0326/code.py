86196db5b42a861f5e38ea3c409eadcf2fd51800
Esther Kim <dptmej0501@naver.com>
2026-03-26 22:18:30 +0900

    2026_03_26_problem_10_solved

---
+++ kest/0326/code.py
@@ -0,0 +1,31 @@
+n = int(input())
+arr = [0] + list(map(int, input().split()))
+
+def heapify(n,i) :
+    largest = i
+    l = i * 2
+    r = i * 2 + 1
+
+    if l <= n and arr[l] > arr[largest]:
+        largest = l
+
+    if r <= n and arr[r] > arr[largest]:
+        largest = r
+
+    if largest != i:
+
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(n, largest)
+
+def heap_sort():
+    for i in range(n // 2, 0 , -1):
+        heapify(n,i)
+
+    for i in range(n, 1, -1):
+        arr[1], arr[i] = arr[i], arr[1]
+        heapify(i -1, 1)
+
+heap_sort()
+
+for elem in arr[1:]:
+    print(elem, end=" ")
